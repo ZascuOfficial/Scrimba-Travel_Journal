86ba18fcdc1f25a6b2cef706a391c7b90d0b79a2
Zascu <zascu.github@gmail.com>
2022-12-27 03:35:30 +0200

    Add an id to every travel item

--- travel-journal/src/js/data/travelItemsData.js
+++ travel-journal/src/js/data/travelItemsData.js
@@ -1,5 +1,6 @@
 const travelItemsData = [
   {
+    id: 1,
     img: 'https://source.unsplash.com/WLxQvbMyfas',
     title: 'Mount Fuji',
     description: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
@@ -10,6 +11,7 @@ const travelItemsData = [
   },
 
   {
+    id: 2,
     img: 'https://source.unsplash.com/JmuyB_LibRo',
     title: 'Sydney Opera House',
     description: 'The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century\'s most famous and distinctive buildings',
@@ -20,6 +22,7 @@ const travelItemsData = [
   },
 
   {
+    id: 3,
     img: 'https://source.unsplash.com/3PeSjpLVtLg',
     title: 'Geirangerfjord',
     description: 'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.',
