92087e00b86e40c3f12dbcc45dd6adb998db80c1
Zascu <zascu.github@gmail.com>
2022-12-27 03:22:23 +0200

    Create the TravelItem.jsx component

---
+++ travel-journal/src/js/components/TravelItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import locationIcon from '../../../public/icons/location-icon.png';
+
+export default function TravelItem(props) {
+  return (
+    <div className="travel-item">
+      <img
+        src={props.img}
+        alt="travel-item-image"
+        className="item-img"
+      />
+
+      <div className="item-details">
+        <img
+          src={locationIcon}
+          alt="location-icon"
+          className="location-icon"
+        />
+
+        <p className="item-location">{props.location}</p>
+        <a href={props.link} className="item-link">View on Google Maps</a>
+
+        <h3 className="item-title">{props.title}</h3>
+
+        <p className="item-dates">
+          {props.startDate} - {props.endDate}
+        </p>
+        <p className="item-description">
+          {props.description}
+        </p>
+      </div>
+    </div>
+  );
+}
